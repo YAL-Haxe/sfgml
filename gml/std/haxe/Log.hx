5eb961c60ef184dbf09c88c5d1e795ecb92a2181
YellowAfterlife <yellowafterlife@ymail.com>
2018-05-27 15:03:32 +0300

    Put gml/std/haxe.Log back because standard Log class wouldn't match properly.

---
+++ gml/std/haxe/Log.hx
@@ -0,0 +1,23 @@
+package haxe;
+
+/**
+ * ...
+ * @author YellowAfterlife
+ */
+class Log {
+	
+	public static function formatOutput( v : Dynamic, infos : PosInfos ) : String {
+		var str = Std.string(v);
+		if (infos == null) return str;
+		var pstr = infos.fileName + ":" + infos.lineNumber;
+		if (infos != null && infos.customParams != null) {
+			for (v in infos.customParams) str += ", " + Std.string(v);
+		}
+		return pstr+": "+str;
+	}
+	
+	public static function trace( v : Dynamic, ?infos : PosInfos ) : Void {
+		gml.Lib.trace(formatOutput(v, infos));
+	}
+
+}
