cdf282972b5c1ce5c0aed5eeffcff409ee5c1de0
YellowAfterlife <yellowafterlife@ymail.com>
2018-05-27 14:52:03 +0300

    Added Display.guiWidth/Height.

--- gml/input/Display.hx
+++ gml/input/Display.hx
@@ -5,7 +5,8 @@ import SfTools.raw;
  * ...
  * @author YellowAfterlife
  */
-@:native("display") extern class Display {
+@:std @:native("display") @:snakeCase
+extern class Display {
 	//
 	static var width(get, never):Int;
 	private static function get_width():Int;
@@ -16,7 +17,7 @@ import SfTools.raw;
 	static function reset(aa:Int, vsync:Bool):Int;
 	static var aaFlags(get, never):Int;
 	private static inline function get_aaFlags():Int return raw("display_aa");
-	//
+	//{
 	static var mouseX(get, never):Float;
 	@:native("mouse_get_x") private static function get_mouseX():Float;
 	//
@@ -24,4 +25,13 @@ import SfTools.raw;
 	@:native("mouse_get_y") private static function get_mouseY():Float;
 	//
 	@:native("mouse_set") static function setMouse(x:Float, y:Float):Void;
+	//}
+	
+	//{
+	static var guiWidth(get, never):Float;
+	private static function get_guiWidth():Float;
+	
+	static var guiHeight(get, never):Float;
+	private static function get_guiHeight():Float;
+	//}
 }
