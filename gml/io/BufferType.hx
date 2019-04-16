e7628cbde64b21939b2b04f5e8da7b35ef9b1738
YellowAfterlife <yellowafterlife@ymail.com>
2019-04-16 13:34:12 +0300

    Try using extern enum abstract for BufferType (for sake of readability)

--- gml/io/BufferType.hx
+++ gml/io/BufferType.hx
@@ -3,23 +3,23 @@ package gml.io;
  * ...
  * @author YellowAfterlife
  */
-@:std @:enum abstract BufferType(Int) from Int to Int {
-	public static function sizeof(type:BufferType):Int {
+@:native("buffer")
+@:std extern enum abstract BufferType(Int) from Int to Int {
+	public static inline function sizeof(type:BufferType):Int {
 		return Buffer.sizeof(type);
 	}
-	//
-	var u8:BufferType = 1;
-	var s8:BufferType = 2;
-	var u16:BufferType = 3;
-	var s16:BufferType = 4;
-	var u32:BufferType = 5;
-	var s32:BufferType = 6;
-	var f16:BufferType = 7;
-	var f32:BufferType = 8;
-	var f64:BufferType = 9;
-	var bool:BufferType = 10;
-	var string:BufferType = 11;
-	/** is actually s64 */
-	var u64:BufferType = 12;
-	var text:BufferType = 13;
+	
+	var u8:BufferType     ; // =  1;
+	var s8:BufferType     ; // =  2;
+	var u16:BufferType    ; // =  3;
+	var s16:BufferType    ; // =  4;
+	var u32:BufferType    ; // =  5;
+	var s32:BufferType    ; // =  6;
+	var f16:BufferType    ; // =  7;
+	var f32:BufferType    ; // =  8;
+	var f64:BufferType    ; // =  9;
+	var bool:BufferType   ; // = 10;
+	var string:BufferType ; // = 11;
+	var u64:BufferType    ; // = 12;
+	var text:BufferType   ; // = 13;
 }
