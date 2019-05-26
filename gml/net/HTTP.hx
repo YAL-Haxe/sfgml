29290b1f4fc2961c5591e9805c31e0856c60781b
YellowAfterlife <yellowafterlife@hotmail.com>
2019-05-26 03:04:50 +0300

    Support calling HTTP.request() with buffer body

--- gml/net/HTTP.hx
+++ gml/net/HTTP.hx
@@ -1,5 +1,6 @@
 package gml.net;
 import gml.ds.HashTable;
+import haxe.extern.EitherType;
 
 /**
  * ...
@@ -11,5 +12,5 @@ extern class HTTP {
 	static function get(url:String):HTTP;
 	@:native("get_file") static function getFile(url:String, localPath:String):HTTP;
 	@:native("post_string") static function postString(url:String, data:String):HTTP;
-	static function request(url:String, method:String, headerMap:HashTable<String, String>, body:String):HTTP;
+	static function request(url:String, method:String, headerMap:HashTable<String, String>, body:EitherType<String, gml.io.Buffer>):HTTP;
 }
