3bd988d9d26d74b0256172753adedecf02d97b4d
YellowAfterlife <yellowafterlife@hotmail.com>
2019-07-30 11:31:07 +0300

    2.2.3 NaN and better checks for finiteness

--- gml/std/Math.hx
+++ gml/std/Math.hx
@@ -1,5 +1,6 @@
 package ;
 import SfTools.raw;
+import gml.NativeType;
 
 /**
  * ...
@@ -37,6 +38,16 @@ import SfTools.raw;
 	static inline function fround(v:Float):Float {
 		return raw("round")(v);
 	}
+	#if (sfgml_version && sfgml_version >= "2.2.3")
+	static inline function isNaN(v:Float):Bool return NativeType.isNaN(v);
+	static inline function isFinite(v:Float):Bool return !NativeType.isNonFinite(v);
+	static var NaN(default, never):Float;
+	@:native("infinity") static var POSITIVE_INFINITY(default, never):Float;
+	static var NEGATIVE_INFINITY(get, never):Float;
+	private static inline function get_NEGATIVE_INFINITY():Float {
+		return -POSITIVE_INFINITY;
+	}
+	#else
 	static inline function isNaN(v:Float):Bool return MathNaN.isNaN(v);
 	static inline function isFinite(v:Float):Bool return MathNaN.isFinite(v);
 	//
@@ -52,7 +63,7 @@ import SfTools.raw;
 	private static inline function get_NEGATIVE_INFINITY():Float {
 		return MathNaN.neg_infinity;
 	}
-	//
+	#end
 }
 @:native("mathnf")
 @:std private class MathNaN {
@@ -60,18 +71,15 @@ import SfTools.raw;
 	public static var pos_infinity = init(1, 0);
 	public static var neg_infinity = init( -1, 0);
 	//
-	static var not_a_number_s = "" + not_a_number;
-	static var pos_infinity_s = "" + pos_infinity;
-	static var neg_infinity_s = "" + neg_infinity;
-	//
 	private static function init(a:Float, b:Float):Float {
 		return (a / b);
 	}
-	public static inline function isNaN(v:Float):Bool {
-		return Std.string(v) == not_a_number_s;
+	public static function isNaN(v:Float):Bool {
+		return v != v;
 	}
 	public static function isFinite(v:Float):Bool {
-		var s = Std.string(v);
-		return s != not_a_number_s && s != pos_infinity_s && s != neg_infinity_s;
+		// NaN is not equal to NaN
+		// inf+1 is equal to inf
+		return v == v && v != v + 1;
 	}
 }
