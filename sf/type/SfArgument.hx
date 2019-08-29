864246515daf7a4654122afc176bbe4b86c2f252
YellowAfterlife <yellowafterlife@hotmail.com>
2019-08-30 00:08:38 +0300

    Added SfArgument to match sfhx change

---
+++ sf/type/SfArgument.hx
@@ -0,0 +1,11 @@
+package sf.type;
+
+import haxe.macro.Type.TConstant;
+
+/**
+ * ...
+ * @author YellowAfterlife
+ */
+class SfArgument extends SfArgumentImpl {
+	public var hidden:Bool = false;
+}
