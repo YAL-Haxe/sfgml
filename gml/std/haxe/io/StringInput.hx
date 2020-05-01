d333987499b8f0bce5cde0dea49b10b81ed4b16e
YellowAfterlife <yellowafterlife@hotmail.com>
2020-05-01 20:29:47 +0300

    Support haxe.io.StringInput without -D sfgml_native_bytes

--- gml/std/haxe/io/StringInput.hx
+++ gml/std/haxe/io/StringInput.hx
@@ -1,4 +1,6 @@
 package haxe.io;
+import gml.NativeArray;
+import gml.NativeString;
 import gml.io.Buffer;
 import gml.io.BufferKind;
 
@@ -6,6 +8,35 @@ import gml.io.BufferKind;
  * ...
  * @author YellowAfterlife
  */
+#if !sfgml_native_bytes
+class StringInput extends Input {
+	//
+	public var position(get, set):Int;
+	private inline function get_position():Int return dataPos;
+	private inline function set_position(p:Int):Int dataPos = p;
+	//
+	public var length(get, never):Int;
+	private inline function get_length():Int return dataLen;
+	//
+	private static inline var new_buf:Buffer = Buffer.defValue;
+	public inline function new(s:String) {
+		var b = new_buf;
+		if (b == null) {
+			b = new Buffer(NativeString.byteLength(s), Grow, 1);
+			new_buf = b;
+		}
+		b.rewind();
+		b.writeChars(s);
+		var n = b.position;
+		var d = NativeArray.createEmpty(n);
+		var i = -1; while (++i < n) {
+			d[i] = b.peekByte(i);
+		}
+		data = d;
+		dataLen = n;
+	}
+}
+#else
 class StringInput extends Input {
 	//
 	public var position(get, set):Int;
@@ -20,3 +51,4 @@ class StringInput extends Input {
 		buf.pokeChars(0, s);
 	}
 }
+#end
