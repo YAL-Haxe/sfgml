4de4a04c843c8a5b7426a8832b4b4adff66c2aa9
YellowAfterlife <yellowafterlife@hotmail.com>
2020-05-01 20:31:39 +0300

    Mostly-@:coreApi compliance for Math

--- gml/std/Math.hx
+++ gml/std/Math.hx
@@ -9,42 +9,49 @@ import gml.NativeType;
 @:native("") @:std extern class Math {
 	@:native("pi") static var PI(default, never):Float;
 	//
-	static function abs(f:Float):Float;
-	static function min(a:Float, b:Float):Float;
-	static function max(a:Float, b:Float):Float;
+	@:pure static function abs(f:Float):Float;
+	@:pure static function min(a:Float, b:Float):Float;
+	@:pure static function max(a:Float, b:Float):Float;
 	//
-	static function sin(r:Float):Float;
-	static function cos(r:Float):Float;
-	static function tan(r:Float):Float;
+	@:pure static function sin(r:Float):Float;
+	@:pure static function cos(r:Float):Float;
+	@:pure static function tan(r:Float):Float;
 	//
-	@:native("arcsin") static function asin(r:Float):Float;
-	@:native("arccos") static function acos(r:Float):Float;
-	@:native("arctan") static function atan(r:Float):Float;
-	@:native("arctan2") static function atan2(y:Float, x:Float):Float;
+	@:pure @:native("arcsin") static function asin(r:Float):Float;
+	@:pure @:native("arccos") static function acos(r:Float):Float;
+	@:pure @:native("arctan") static function atan(r:Float):Float;
+	@:pure @:native("arctan2") static function atan2(y:Float, x:Float):Float;
 	//
-	static function exp(f:Float):Float;
-	@:native("ln") static function log(f:Float):Float;
-	@:native("power") static function pow(f:Float, e:Float):Float;
-	static function sqrt(f:Float):Float;
+	@:pure static function exp(f:Float):Float;
+	@:pure @:native("ln") static function log(f:Float):Float;
+	@:pure @:native("power") static function pow(f:Float, e:Float):Float;
+	@:pure static function sqrt(f:Float):Float;
 	//
-	static function round(f:Float):Int;
-	static function floor(f:Float):Int;
-	static function ceil(f:Float):Int;
+	@:pure static function round(f:Float):Int;
+	@:pure static function floor(f:Float):Int;
+	@:pure static function ceil(f:Float):Int;
 	//
 	static inline function random():Float {
 		return raw("random")(1);
 	}
 	//
-	static inline function fround(v:Float):Float {
+	@:pure static inline function ffloor(v:Float):Float {
+		return floor(v);
+	}
+	@:pure static inline function fceil(v:Float):Float {
+		return ceil(v);
+	}
+	@:pure static inline function fround(v:Float):Float {
 		return raw("round")(v);
 	}
+	
 	#if (sfgml_version && sfgml_version >= "2.2.3")
-	static inline function isNaN(v:Float):Bool return NativeType.isNaN(v);
-	static inline function isFinite(v:Float):Bool return !NativeType.isNonFinite(v);
+	@:pure static inline function isNaN(v:Float):Bool return NativeType.isNaN(v);
+	@:pure static inline function isFinite(v:Float):Bool return !NativeType.isNonFinite(v);
 	static var NaN(default, never):Float;
 	@:native("infinity") static var POSITIVE_INFINITY(default, never):Float;
 	static var NEGATIVE_INFINITY(get, never):Float;
-	private static inline function get_NEGATIVE_INFINITY():Float {
+	@:pure private static inline function get_NEGATIVE_INFINITY():Float {
 		return -POSITIVE_INFINITY;
 	}
 	#else
@@ -52,15 +59,15 @@ import gml.NativeType;
 	static inline function isFinite(v:Float):Bool return MathNaN.isFinite(v);
 	//
 	static var NaN(get, never):Float;
-	private static inline function get_NaN():Float {
+	@:pure private static inline function get_NaN():Float {
 		return MathNaN.not_a_number;
 	}
 	static var POSITIVE_INFINITY(get, never):Float;
-	private static inline function get_POSITIVE_INFINITY():Float {
+	@:pure private static inline function get_POSITIVE_INFINITY():Float {
 		return MathNaN.pos_infinity;
 	}
 	static var NEGATIVE_INFINITY(get, never):Float;
-	private static inline function get_NEGATIVE_INFINITY():Float {
+	@:pure private static inline function get_NEGATIVE_INFINITY():Float {
 		return MathNaN.neg_infinity;
 	}
 	#end
