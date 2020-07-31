460915e928acf4f154f0525aeca562b851dfab1e
YellowAfterlife <yellowafterlife@hotmail.com>
2020-07-31 10:28:38 +0300

    Added externs for Tileset

--- gml/assets/Tileset.hx
+++ gml/assets/Tileset.hx
@@ -1,4 +1,5 @@
 package gml.assets;
+import gml.gpu.Texture;
 
 /**
  * ...
@@ -6,4 +7,27 @@ package gml.assets;
  */
 @:native("tileset") extern class Tileset extends Asset {
 	public static inline var defValue:Tileset = cast -1;
+	
+	public static inline function fromIndex(i:Int):Tileset return cast i;
+	
+	public static inline function isValid(ts:Tileset):Bool {
+		// aha, mhm
+		return ts.name.charCodeAt(0) != "<".code;
+	}
+	
+	//
+	var index(get, never):Int;
+	private inline function get_index():Int return cast this;
+	
+	//
+	var name(get, never):String;
+	private function get_name():String;
+	
+	//
+	var texture(get, never):Texture;
+	private function get_texture():Texture;
+	
+	//
+	var uvs(get, never):Array<Float>;
+	private function get_uvs():Array<Float>;
 }
