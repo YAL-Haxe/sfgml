fbbbe8cd351414e04735329f0b8ed182b0fd4e4a
YellowAfterlife <yellowafterlife@hotmail.com>
2020-08-20 17:56:04 +0300

    There's no shader_exists

--- gml/assets/Shader.hx
+++ gml/assets/Shader.hx
@@ -4,9 +4,12 @@ package gml.assets;
 extern class Shader extends Asset {
 	static inline var defValue:Sprite = cast -1;
 	//
-	@:native("exists") static function isValid(q:Shader):Bool;
-	//
 	static inline function fromIndex(i:Int):Shader return cast i;
+	
+	public static inline function isValid(sh:Shader):Bool {
+		// aha, mhm
+		return sh.name.charCodeAt(0) != "<".code;
+	}
 	//{
 	/** GMS2 only */
 	var name(get, never):String;
