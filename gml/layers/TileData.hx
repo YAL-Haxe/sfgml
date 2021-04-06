90a3dd1b41530db7984fa8e1abcff6b02e9aea03
YellowAfterlife <yellowafterlife@hotmail.com>
2021-04-06 21:59:53 +0300

    Added more externs for GMS2 tile functions

--- gml/layers/TileData.hx
+++ gml/layers/TileData.hx
@@ -4,6 +4,15 @@ package gml.layers;
  * ...
  * @author YellowAfterlife
  */
+@:using(gml.layers.TileData.TileDataHelpers)
 abstract TileData(Int) from Int to Int {
 	
+}
+@:native("tile") @:snakeCase
+extern class TileDataHelpers {
+	static function getEmpty(td:TileData):Bool;
+	static function getIndex(td:TileData):Int;
+	static function getFlip(td:TileData):Bool;
+	static function getMirror(td:TileData):Bool;
+	static function getRotate(td:TileData):Bool;
 }
--- gml/layers/Tilemap.hx
+++ gml/layers/Tilemap.hx
@@ -14,8 +14,33 @@ extern class Tilemap extends LayerElement {
 	@:expose("layer_tilemap_destroy") public function destroy():Void;
 	//
 	public function get(x:Int, y:Int):TileData;
-	@:native("set") private function setImpl(t:TileData, x:Int, y:Int):Void;
+	@:native("set") private function __set(t:TileData, x:Int, y:Int):Void;
 	public inline function set(x:Int, y:Int, tile:TileData):Void {
-		setImpl(tile, x, y);
+		__set(tile, x, y);
 	}
+	
+	@:native("set_at_pixel") private function __setAtPixel(t:TileData, x:Float, y:Float):Void;
+	public inline function setAtPixel(x:Float, y:Float, tile:TileData):Void {
+		__setAtPixel(tile, x, y);
+	}
+	
+	public var x(get, set):Float;
+	private function get_x():Float;
+	private inline function set_x(val:Float):Float {
+		__x(val); return val;
+	}
+	@:native("x") private function __x(val:Float):Void;
+	
+	public var y(get, set):Float;
+	private function get_y():Float;
+	private inline function set_y(val:Float):Float {
+		__y(val); return val;
+	}
+	@:native("y") private function __y(val:Float):Void;
+	
+	public var tileWidth(get, never):Int;
+	private function get_tileWidth():Int;
+	
+	public var tileHeight(get, never):Int;
+	private function get_tileHeight():Int;
 }
