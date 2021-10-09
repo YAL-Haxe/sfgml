385ef9ee93eb48b86fa1274a6e9e873037a5fac1
YellowAfterlife <yellowafterlife@hotmail.com>
2021-10-09 16:26:40 +0300

    Use native constants for gamepad buttons

--- gml/input/GamepadButton.hx
+++ gml/input/GamepadButton.hx
@@ -1,20 +1,22 @@
 package gml.input;
 
-@:enum abstract GamepadButton(Int) from Int to Int {
-	var Face1 = 0x8001;
-	var Face2 = 0x8002;
-	var Face3 = 0x8003;
-	var Face4 = 0x8004;
-	var ShoulderL = 0x8005;
-	var ShoulderR = 0x8006;
-	var TriggerL = 0x8007;
-	var TriggerR = 0x8008;
-	var Select = 0x8009;
-	var Start = 0x800A;
-	var StickL = 0x800B;
-	var StickR = 0x800C;
-	var PadU = 0x800D;
-	var PadD = 0x800E;
-	var PadL = 0x800F;
-	var PadR = 0x8010;
+@:docName("gamepad_button")
+@:native("gp") @:snakeCase
+extern enum abstract GamepadButton(Int) from Int to Int {
+	var Face1;
+	var Face2;
+	var Face3;
+	var Face4;
+	@:native("shoulderl") var ShoulderL;
+	@:native("shoulderr") var ShoulderR;
+	@:native("shoulderlb") var TriggerL;
+	@:native("shoulderrb") var TriggerR;
+	var Select;
+	var Start;
+	@:native("stickl") var StickL;
+	@:native("stickr") var StickR;
+	@:native("padu") var PadU;
+	@:native("padd") var PadD;
+	@:native("padl") var PadL;
+	@:native("padr") var PadR;
 }
