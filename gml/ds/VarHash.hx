20b679bdb885107146c20be77cb52ecf30a46655
YellowAfterlife <yellowafterlife@hotmail.com>
2025-07-01 02:19:25 +0300

    API tweaks (collision functions now take arrays, hash functions have an `exists`)

--- gml/ds/VarHash.hx
+++ gml/ds/VarHash.hx
@@ -14,11 +14,20 @@ extern class VarHash {
 	@:expose("struct_set_from_hash")
 	public static function set(to:Dynamic, hash:VarHash, value:Dynamic):Void;
 	
+	@:expose("struct_exists_from_hash")
+	public static function exists(struct:Dynamic, hash:VarHash):Bool;
+	
+	@:expose("struct_remove_from_hash")
+	public static function remove(struct:Dynamic, hash:VarHash):Void;
+	
 	public inline function readFrom(from:Dynamic):Dynamic {
 		return get(from, this);
 	}
 	public inline function writeTo(to:Dynamic, value:Dynamic):Void {
 		set(to, this, value);
+	}
+	public inline function existsIn(struct:Dynamic){
+		return exists(struct, this);
 	}
 	//
 	public inline function toInt():Int {
--- gml/physics/Physics.hx
+++ gml/physics/Physics.hx
@@ -1,4 +1,5 @@
 package gml.physics;
+import gml.Instance;
 import gml.assets.Object;
 import gml.ds.ArrayList;
 import haxe.extern.EitherType;
@@ -11,56 +12,60 @@ import haxe.extern.EitherType;
 extern class Physics {
 	@:expose("collision_point")
 	static function pointCast<T:Instance>(x:Float, y:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool
 	):T;
 	
 	@:expose("collision_point_list")
 	static function pointCastList<T:Instance>(x:Float, y:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool,
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool,
 		list:ArrayList<T>, ordered:Bool
 	):Int;
 	
 	@:expose("collision_circle")
 	static function circleCast<T:Instance>(x:Float, y:Float, rad:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool
 	):T;
 	
 	@:expose("collision_circle_list")
 	static function circleCastList<T:Instance>(x:Float, y:Float, rad:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool,
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool,
 		list:ArrayList<T>, ordered:Bool
 	):Int;
 	
 	@:expose("collision_line")
 	static function lineCast<T:Instance>(x1:Float, y1:Float, x2:Float, y2:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool
 	):T;
 	
 	@:expose("collision_line_list")
 	static function lineCastList<T:Instance>(x1:Float, y1:Float, x2:Float, y2:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool,
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool,
 		list:ArrayList<T>, ordered:Bool
 	):Int;
 	
 	@:expose("collision_rectangle")
 	static function rectCast<T:Instance>(x1:Float, y1:Float, x2:Float, y2:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool
 	):T;
 	
 	@:expose("collision_rectangle_list")
 	static function rectCastList<T:Instance>(x1:Float, y1:Float, x2:Float, y2:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool,
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool,
 		list:ArrayList<T>, ordered:Bool
 	):Int;
 	
 	@:expose("collision_ellipse")
 	static function ellipseCast<T:Instance>(x1:Float, y1:Float, x2:Float, y2:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool
 	):T;
 	
 	@:expose("collision_ellipse_list")
 	static function ellipseCastList<T:Instance>(x1:Float, y1:Float, x2:Float, y2:Float,
-		obj:EitherType<ObjectOf<T>, T>, prec:Bool, notme:Bool,
+		obj:PhysicsCastTarget<T>, prec:Bool, notme:Bool,
 		list:ArrayList<T>, ordered:Bool
 	):Int;
 }
+typedef PhysicsCastTarget<T:Instance> = EitherType<
+	EitherType<T, ObjectOf<T>>,
+	Array<EitherType<T, ObjectOf<T>>>
+>;
